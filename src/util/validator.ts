1f293973482d16337415519e87ab16a89b092c28
jbostik <ebiven1563@gmail.com>
2020-05-03 17:57:36 -0400

    Added documentation to validator

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -1,11 +1,21 @@
+/**
+ * Checks that the id passed is not falsy, is a number, and is an integer.
+ */
 export const isValidId = (id: number): boolean => {
-    return !!(id && typeof id === 'number' && Number.isInteger(id) && id > 0);
+    return (id && typeof id === 'number' && Number.isInteger(id) && id > 0);
 };
 
+/**
+ * Checks to see that all strings in an array are not falsy and are string types.
+ */
 export const isValidStrings = (...strs: string[]): boolean => {
     return (strs.filter(str => !str || typeof str !== 'string').length == 0);
 };
 
+/**
+ * Checks to see that an object is valid by ensuring it is not falsy, not
+ * empty, and all properties of the object have values.
+ */
 export const isValidObject = (obj: Object, ...nullableProps: string[]) => {
     return obj && Object.keys(obj).every(key => {
         if (nullableProps.includes(key)) return true;
@@ -13,6 +23,11 @@ export const isValidObject = (obj: Object, ...nullableProps: string[]) => {
     });
 };
 
+/**
+ * Checks to see if an object is of the specified type by creating an
+ * object of the specified type and comparing it to the passed type
+ * parameter.
+ */
 export const isPropertyOf = (prop: string, type: any) => {
 
     if (!prop || !type) {
@@ -34,6 +49,10 @@ export const isPropertyOf = (prop: string, type: any) => {
 
 }
 
+/**
+ * Checks to make sure an object is not empty by ensuring it is
+ * not falsy and is not an empty object.
+ */
 export function isEmptyObject<T>(obj: T) {
     return obj && Object.keys(obj).length === 0;
 }
